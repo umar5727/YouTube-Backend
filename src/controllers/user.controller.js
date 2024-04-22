5968a357cf81fb947b6dbd7d1a526da19bb83fb0
Umar Khan <56593023+umarkhan1010@users.noreply.github.com>
2024-04-23 02:31:49 +0530

    getting data from user using postman and printing user data on console

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,12 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'first response '
-    })
+    // res.status(200).json({
+    //     message: 'first response '
+    // })
+
+    const { fullName, email, userName, password, avatar, coverImage } = req.body
+    console.log("fullname: " + fullName + " email: " + email)
 })
 
-export {registerUser}
+export { registerUser }
